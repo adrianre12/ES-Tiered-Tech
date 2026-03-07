c3167ccd3d8fcd355aa9204343cc84c902b1aaa3
adrianre12 <adrianrevill@hotmail.com>
2026-03-07 12:58:30 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# ES Tiered Tech
+
+This is a modified version of Tiered Tech Blocks Beyond https://steamcommunity.com/sharedfiles/filedetails/?id=3642466709 for use on the Everdawn Server.
---
+++ TorchDeploy.cmd
@@ -0,0 +1,19 @@
+@REM exit 0
+exit 0
+set MOD_NAME="ES Tiered Tech"
+set MOD_ID=3649354694
+
+set SE_CONTENT_DIR=N:\SteamLibrary\steamapps\workshop\content\244850
+REM set TORCH_CONTENT_DIR=G:\qPanel\Users\kristoffere\2092634\Instance\content\244850
+set TORCH_CONTENT_DIR=G:\torch-server\Instance\content\244850
+
+
+set MOD_DIR=%APPDATA%\SpaceEngineers\Mods
+
+rmdir %SE_CONTENT_DIR%\%MOD_ID% /S /Q 
+robocopy.exe %MOD_DIR%\%MOD_NAME%\ %SE_CONTENT_DIR%\%MOD_ID%\ "*.*" /S -xf *.sbmi
+
+rmdir %TORCH_CONTENT_DIR%\%MOD_ID% /S /Q 
+robocopy.exe %MOD_DIR%\%MOD_NAME%\ %TORCH_CONTENT_DIR%\%MOD_ID%\ "*.*" /S -xf *.sbmi
+
+echo Done
